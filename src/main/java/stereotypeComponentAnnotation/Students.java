b5b4db55e14e72123dbf2dedf1f898b291e49fc2
shivam <shivamkr758@gmail.com>
2023-09-10 22:37:03 +0530

    value annotation use without property use in bean in config file

--- src/main/java/stereotypeComponentAnnotation/Students.java
+++ src/main/java/stereotypeComponentAnnotation/Students.java
@@ -1,5 +1,7 @@
 package stereotypeComponentAnnotation;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,16 @@ public class Students {
 	private String name;
 @Value("11")
 	private int roll;
+@Value("#{deg}")
+private List<String>degree;
+
+
+public List<String> getDegree() {
+	return degree;
+}
+public void setDegree(List<String> degree) {
+	this.degree = degree;
+}
 public String getName() {
 	return name;
 }
@@ -23,7 +35,7 @@ public void setRoll(int roll) {
 }
 @Override
 public String toString() {
-	return "Student [name=" + name + ", roll=" + roll + "]";
+	return "Students [name=" + name + ", roll=" + roll + ", degree=" + degree + "]";
 }
 
 
