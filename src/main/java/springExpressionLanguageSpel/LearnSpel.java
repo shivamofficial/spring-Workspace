b009086fe8f976e546c050895b1c4fec5be29d27
shivam <shivamkr758@gmail.com>
2023-09-13 23:36:15 +0530

    spel part 2

--- src/main/java/springExpressionLanguageSpel/LearnSpel.java
+++ src/main/java/springExpressionLanguageSpel/LearnSpel.java
@@ -4,38 +4,61 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component("spel") // create bean
 public class LearnSpel {
-
+     @Value("#{22}")
 	private Integer userAge;  
+     
+	@Value("#{'India'}")
 	private String userCountry;
+	@Value("#{22+55}")
 	private Integer myNum; 
+	@Value("#{22>55}")
 	private Boolean userActive; 
+	
+	@Value("#{l}")
 	private List<String> userSkills; 
+	
+	@Value("#{{101:'Mahesh',102:'Surya'}}")
 	private Map<Integer, String> teamMates; 
+	
 	@Value("#{(10 < 15)}")
 	private Boolean myVal1; // true 
 	@Value("#{(30 > 15)}")
 	private Boolean myVal2; // true 
-	@Value("#{!(12 <= 18)}") // false
+	@Value("#{!(12 <= 18)}") 
 	private Boolean myVal3; 
-	@Value("#{address.country == 'India'}")
-	private Boolean myVal4; 
+	
 	@Value("#{10 > 6 && 15 < 20}") 
-	private Boolean isUserActive; // true 
+	private Boolean isUserActive; 
 	@Value("#{10 > 15 || 15 < 20}")
-	private Boolean isUserActive1; // true 
+	private Boolean isUserActive1; 
 	@Value("#{!(10 > 15)}")
-	private Boolean isUserActive2; //false 
+	private Boolean isUserActive2;
 	@Value("#{15 + 10}")
-	private Integer myVal7; // 25 
+	private Integer myVal7;
 	@Value("#{150 - 100}")
-	private Integer myVal5; // 50 
+	private Integer myVal5; 
 	@Value("#{45 / 9}")
-	private Integer myVal6; // 5 
+	private Integer myVal6; 
+	
+	@Value("#{ T(java.lang.Math).random() * 100}") // The T operator is used to specify an instance of java.lang.Class.
+	private Integer randomNumber;
 
+	@Override
+	public String toString() {
+		return "LearnSpel [userAge=" + userAge + ", userCountry=" + userCountry + ", myNum=" + myNum + ", userActive="
+				+ userActive + ", userSkills=" + userSkills + ", teamMates=" + teamMates + ", myVal1=" + myVal1
+				+ ", myVal2=" + myVal2 + ", myVal3=" + myVal3 + ", isUserActive=" + isUserActive + ", isUserActive1="
+				+ isUserActive1 + ", isUserActive2=" + isUserActive2 + ", myVal7=" + myVal7 + ", myVal5=" + myVal5
+				+ ", myVal6=" + myVal6 + ", randomNumber=" + randomNumber + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	} 
 	
 	
+
 	
 	
 }
--- src/main/java/springExpressionLanguageSpel/Test.java
+++ src/main/java/springExpressionLanguageSpel/Test.java
@@ -1,9 +1,18 @@
 package springExpressionLanguageSpel;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ApplicationContext context =new ClassPathXmlApplicationContext("springExpressionLanguageSpel/spelConfig.xml");
+		
+		LearnSpel obj1= (LearnSpel)context.getBean("spel");
+		
+		System.out.println(obj1);
 
 	}
 
